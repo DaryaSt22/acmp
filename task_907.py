dc6571d0aa0dab432af8774d6f0ba630e0eaa95f
Darya <ara1015@tut.by>
2026-03-17 12:30:42 +0200

    task 23, 907

---
+++ task_907.py
@@ -0,0 +1,19 @@
+# Удасться ли спрятать круглый коврик под прямоугольным ковриком?
+# 1. Найдите диаметр круга
+# # Если известен R радиус, то D = 2 * R
+#
+w, h, r = map(int, input("Введите три числа через пробел: ").split())
+circle_diameter = 2 * r
+# # Прямоугольник со сторонами a > D, b > D
+# # Круг помещается свободно.
+if circle_diameter <= min(w, h):
+    print("Yes")
+else:
+    print("No")
+
+w, h, r = map(int, input("Введите три числа через пробел: ").split())
+circle_diameter = 2 * r
+if circle_diameter <= min(w, h):
+    print("Yes")
+else:
+    print("No")
